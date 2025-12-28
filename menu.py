d032b6dcb3dacfbedb923a1c5c6462a48c2d93c0
vishalsharma-eng <vishalkumarsharma016@gmail.com>
2025-12-28 15:46:47 +0530

    Delete menu.py

--- menu.py
+++
@@ -1,9 +0,0 @@
-def show_menu():
-    print("\n========== PERSONAL FINANCE MANAGER ==========")
-    print("1. Add New Expense")
-    print("2. View All Expenses")
-    print("3. View Category-wise Summary")
-    print("4. Generate Monthly Report")
-    print("5. Search Expenses")
-    print("6. Backup Data")
-    print("7. Exit")
